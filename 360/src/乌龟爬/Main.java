1e6df0c47c4319b9a7c26c523614ad1d8126afae
JiangKunZhang <1723442668@qq.com>
2020-12-13 21:39:41 +0800

    360

---
+++ 360/src/乌龟爬/Main.java
@@ -0,0 +1,12 @@
+package 乌龟爬;
+
+/**
+ * @author PineappleSnow
+ * @version 1.0
+ * @date 2020/9/26 19:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(3);
+    }
+}
---
+++ 360/src/装载货物/Main.java
@@ -0,0 +1,23 @@
+package 装载货物;
+
+import java.util.Scanner;
+
+/**
+ * @author PineappleSnow
+ * @version 1.0
+ * @date 2020/9/26 19:58
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int k = sc.nextInt();
+            int v = sc.nextInt();
+            
+        }
+        System.out.println(7);
+        System.out.println(3);
+    }
+}
